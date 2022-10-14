463e992b9d6fa0522d3dbb151f126ee91fbb90ee
zhanshuo.qi <245063446@qq.com>
2022-10-14 16:42:28 +0800

    test(TodoListApi.Tests): Add TestProject

---
+++ TodoListApi.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TodoListApi.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
